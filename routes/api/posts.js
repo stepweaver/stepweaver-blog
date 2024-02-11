0ddca636f1736c14324fb9c6b7242c0e13af8d7e
stepweaver <weaverst@mail.gvsu.edu>
2024-02-11 08:38:58 -0500

    Add Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route   GET api/posts
-// @desc    Test posts route
-// @access  Public
-router.get('/', (req, res) => {
-  res.send('Posts route');
-});
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').notEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { id } = req.user;
+      const { text } = req.body; // Extract text from request body
+      const user = await User.findById(id).select('-password');
+
+      const newPost = new Post({
+        text,
+        name: user.name,
+        avatar: user.avatar,
+        user: id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: 'Server Error', error: err.message });
+    }
+  }
+);
 
 module.exports = router;
