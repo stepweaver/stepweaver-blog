ddf12e139fac3bbe7b66bffd3655abb6e55e6abf
stepweaver <weaverst@mail.gvsu.edu>
2024-02-11 10:41:09 -0500

    Add & Remove Comment Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -161,4 +161,79 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post('/comment/:id',
+  [auth, [check('text', 'Text is required').notEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+    try {
+      const { id, name, avatar } = req.user;
+      const { text } = req.body;
+      const user = await User.findById(id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text,
+        name,
+        avatar,
+        user: id
+      };
+
+      post.comments.unshift(newComment);
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send({ msg: 'Server Error', error: err.message });
+    }
+  }
+);
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.id)
+      .indexOf(req.params.comment_id);
+
+    if (removeIndex !== -1) {
+      post.comments.splice(removeIndex, 1);
+      await post.save();
+    }
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send({ msg: 'Server Error', error: err.message });
+  }
+});
+
 module.exports = router;
