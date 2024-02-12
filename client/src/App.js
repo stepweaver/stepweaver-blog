8438382289f41b805fdb77879b418863a8e8df1d
stepweaver <weaverst@mail.gvsu.edu>
2024-02-12 13:40:38 -0500

    Create Redux Store (configureStore)
    Install tailwindCss & daisyui

--- client/src/App.js
+++ client/src/App.js
@@ -4,31 +4,37 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Landing />} />
-      <Route
-        path='/register'
-        element={
-          <section className='container'>
-            <Register />
-          </section>
-        }
-      />
-      <Route
-        path='/login'
-        element={
-          <section className='container'>
-            <Login />
-          </section>
-        }
-      />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route
+          path='/register'
+          element={
+            <section className='container'>
+              <Register />
+            </section>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <section className='container'>
+              <Login />
+            </section>
+          }
+        />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
