e511233d0a597e5a4eb04a0a1d60d757339f3e74
stepweaver <weaverst@mail.gvsu.edu>
2024-03-04 15:09:43 -0500

    Update server.js to listen on all network interfaces

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
